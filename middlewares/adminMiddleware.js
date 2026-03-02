808e65bedbf4cc079264cf8d2e80da0f2877033f
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2026-03-02 22:42:37 +0200

    Folder holding the middlewares of my app.

---
+++ middlewares/adminMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken")
+const User = require("../schema/userSchema")
+
+const adminMiddleware = async (req, res, next) => {
+
+    const token = req.cookies.token
+    const jwtsecret = process.env.JWT_SECRET
+
+    if (!token) {
+        return res.status(401).json({ message: "Please login or register to continue." })
+    }
+
+    try {
+        const verifiedToken = jwt.verify(token, jwtsecret)
+        if (!verifiedToken) {
+            return res.status(401).json({ message: "Secret Invalid" })
+        }
+
+        const user = await User.findById({_id: verifiedToken.id}).select("-password")
+        if (!user || !user.admin) {
+            return res.status(401).json({ message: "Invalid ID or not authorized to be admin" })
+        }
+
+        req.user = user
+        next()
+
+    } catch (error) {
+       console.log(error) 
+       return res.status(401).json({ message: "Token invalid or expired" })
+    }
+
+}
+
+module.exports = adminMiddleware
